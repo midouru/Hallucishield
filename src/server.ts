cef1d83debde8f06b1c45a7ebe1c3637bf53dfbc
Mridul Tripathi <mridultripathi869@gmail.com>
2026-06-01 23:53:21 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import app from './app';
-import { loadFactEmbeddings } from './knowledge/factembeddings';
+/*import { loadFactEmbeddings } from './knowledge/factembeddings';*/
+import { loadFactstoChroma } from './database/loadfacts';
 
 async function startServer() {
-    await loadFactEmbeddings();
+    /*await loadFactEmbeddings();*/
+    await loadFactstoChroma();
     app.listen(3000, () => {
         console.log('Server is running on port 3000');
     });
